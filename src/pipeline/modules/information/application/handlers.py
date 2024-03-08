f70665961f3ab44e5de7190da0e5b89b4231fe6d
robertoamin <r.amin@uniandes.edu.co>
2024-03-08 17:12:14 -0500

    se agrega datalake

--- src/pipeline/modules/information/application/handlers.py
+++ src/pipeline/modules/information/application/handlers.py
@@ -3,5 +3,9 @@ from src.pipeline.seedwork.application.handlers import Handler
 
 class DomainEventHandler(Handler):
     @staticmethod
-    def handle_enriched_information(self, event):
+    def handle_data_complete(self, event):
+        raise NotImplementedError()
+
+    @staticmethod
+    def handle_png_map_present(self, event):
         raise NotImplementedError()
